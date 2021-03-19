00b056942de581bb103a177ae9b719e3ba3bb95b
mtheu <mtheu@DESKTOP-D4T97T6>
2021-03-18 21:04:42 -0300

    Other entities and second migration

--- SalesWebMVC/Models/Department.cs
+++ SalesWebMVC/Models/Department.cs
@@ -9,5 +9,27 @@ namespace SalesWebMvc.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        public Department()
+        {
+
+        }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSeller(Seller sl)
+        {
+            Sellers.Add(sl);
+        }
+
+        public double TotalSales(DateTime initialDate, DateTime finalDate)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(initialDate, finalDate));
+        }
     }
 }
